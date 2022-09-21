64f9e5c983e82deaa3f6c2b6a63971d5b5584584
Sunanda Wasala <sunanda@listudiosl.com>
2022-09-21 08:09:20 +0530

    push  code

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+require('./bootstrap')
+
+import { createApp } from 'vue';
+import Welcome from './components/welcome';
+
+const app = createApp({})
+
+app.component('welcome', Welcome)
+
+app.mount('#app')
