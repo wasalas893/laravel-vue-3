7f8ff5f58624e00f096ac76fbd758b75ca91c6e9
Sunanda Wasala <sunanda@listudiosl.com>
2022-09-23 10:25:30 +0530

    create pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,11 +10,19 @@ import App from './app.vue';
 import faq from './components/faq';
 import Home from './components/home.vue';
 import journal from './components/journal.vue';
+import process from './components/process.vue';
+import review from './components/review.vue';
+import company from './components/company.vue';
+import contact from './components/contact.vue';
 
 const routes = [
     {path: "/faq",name: "faq",component: faq},
     {path: "/",name: "Home",component: Home},
     {path: "/journal",name: "journal",component: journal},
+    {path: "/process",name: "process",component: process},
+    {path: "/review",name: "review",component: review},
+    {path: "/company",name: "company",component: company},
+    {path: "/contact",name: "contact",component: contact},
   ];
   
 
